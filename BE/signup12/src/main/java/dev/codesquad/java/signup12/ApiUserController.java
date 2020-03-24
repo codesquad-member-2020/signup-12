27d75b4c82e85136137fdf6d6be80929afda7138
wooody92 <pkower4@naver.com>
2020-03-24 14:41:11 +0900

    feat : 임시 회원가입 페이지 동작 확인
    - templates/form.html 페이지 동작 확인
    - ApiUserController.java
    - UserController.java : viewUserForm()
        - return 값으로 /form.html 전달
    
    associate with #7

---
+++ BE/signup12/src/main/java/dev/codesquad/java/signup12/ApiUserController.java
@@ -0,0 +1,18 @@
+package dev.codesquad.java.signup12;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiUserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/")
+    public User test() {
+        Long id = 1L;
+        return userRepository.findById(id).orElseThrow(() -> new IllegalStateException("NO DATA"));
+    }
+}
--- BE/signup12/src/main/java/dev/codesquad/java/signup12/UserController.java
+++ BE/signup12/src/main/java/dev/codesquad/java/signup12/UserController.java
@@ -1,18 +1,13 @@
 package dev.codesquad.java.signup12;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class UserController {
 
-    @Autowired
-    UserRepository userRepository;
-
-    @GetMapping("/")
-    public User test() {
-        Long id = 1L;
-        return userRepository.findById(id).orElseThrow(() -> new IllegalStateException("NO DATA"));
+    @GetMapping("/test")
+    public String viewUserForm() {
+        return "/form";
     }
 }
