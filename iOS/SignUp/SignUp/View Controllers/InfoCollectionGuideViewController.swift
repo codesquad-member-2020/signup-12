e99d56e9865f2e054b63e646f53d9d22af7a9d42
MagnaPax <MagnaPax@outlook.kr>
2020-03-27 02:57:10 +0900

    각 화면 만들기
    각 화면 파일 생성 #11

---
+++ iOS/SignUp/SignUp/View Controllers/InfoCollectionGuideViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InfoCollectionGuideViewController.swift
+//  SignUp
+//
+//  Created by 한천희 on 2020/03/27.
+//  Copyright © 2020 한천희. All rights reserved.
+//
+
+import UIKit
+
+class InfoCollectionGuideViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
