1281b948ebbc876f9ccc12ead8e66d379bab1dbb
wooody92 <pkower4@naver.com>
2020-03-27 16:39:32 +0900

    feat : 관심사 컬럼 추가
    - User.java : interest 컬럼 추가했습니다.
     - 관계형 DB 기능은 업데이트 예정입니다.
    
    associate with #7

--- BE/signup12/src/main/resources/data.sql
+++ BE/signup12/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into user(user_id, password, name, email, gender, phone, created_date, birth_date) values ('wooody92', '123', 'Henry', 'wooody92@gmail.com', '남', '01012345678', getdate(), '1999-01-01');
+insert into user(user_id, password, name, email, gender, phone, created_date, birth_date, interest) values ('wooody92', '123', 'Henry', 'wooody92@gmail.com', '남', '01012345678', getdate(), '1999-01-01', 'a,b,c');
 insert into user(user_id, password, name, email, gender, phone, created_date, birth_date) values ('hamil1', '123', 'hamill', 'abc@gmail.com', '남', '01011111111', getdate(), '1999-01-01');
 
